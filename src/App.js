3243214d4e9a5b8bb2df51549faa796396c7e6ad
김동은 <ehddms0517@naver.com>
2025-10-26 00:51:01 +0900

    feat(App): 자동차 이름을 쉼표를 기준으로 분리

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import input from './utils/input.js';
 class App {
   async run() {
     const { carNames, count } = await input();
+
+    const cars = carNames.split(`,`).map((name) => name.trim());
   }
 }
 
