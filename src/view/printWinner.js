418ef4429d2f3e695d9c4e6a501598501745959a
김동은 <ehddms0517@naver.com>
2025-10-27 13:47:41 +0900

    fix(printWinner): 출력 공백 출력

--- src/view/printWinner.js
+++ src/view/printWinner.js
@@ -3,7 +3,7 @@ import getWinner from '../service/getWinner.js';
 
 const printWinner = (cars) => {
   const winners = getWinner(cars);
-  Console.print(`최종 우승자: ${winners.join(', ')}`);
+  Console.print(`최종 우승자 : ${winners.join(', ')}`);
 };
 
 export default printWinner;
